068b04dee91b3f9cb8f4f63a0dcdfd83bff66dc5
AgustinWaves <agustinacostaolivera@gmail.com>
2022-06-15 00:53:40 +0100

    fixed link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
 
-const API_URL = 'http://www.omdbapi.com/?apikey=c7ced9d5&';
+const API_URL = 'https://www.omdbapi.com/?apikey=c7ced9d5&';
 
 
 const App = () => {
